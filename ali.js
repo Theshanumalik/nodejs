4e5a1c1e8d56d59d1e22fb057ad37475d57e0a77
theshanumalik <shanumalik9001567.com>
2023-03-28 21:57:46 +0530

    first class

---
+++ ali.js
@@ -0,0 +1,47 @@
+// console.log("Hello world");
+
+/*
+    VARIABLES
+    CONDITIONS
+    LOOPS
+    DATATYPES
+*/
+
+// let name = "Ali Chauhan";
+// let a = 87;
+
+// var schoolName = "APJ";
+
+// console.log(schoolName);
+
+/*
+    String "Ali Chauhan", "Tosif khan"
+    Number 12, 4, -8, 8.88, 3.14
+    Array [1,2,3,4]
+    Boolean true false
+*/
+
+// let num1 = 22;
+// let num2 = 3;
+
+// let sum = num1 % num2;
+
+// console.log(sum);
+
+// let name = "Tosif khan";
+
+// if (name == "Tosif khn") {
+//   console.log("you are admin");
+// } else {
+//   console.log("you are not an admin");
+// }
+
+let year = 2016;
+
+let reminder = year % 4;
+
+if (reminder == 0) {
+  console.log("it's a leap year");
+} else {
+  console.log("it's not a leap year");
+}
