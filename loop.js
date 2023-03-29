db13c014918d942087ae87e3956f7bc0ab303b0b
theshanumalik <shanumalik9001567.com>
2023-03-29 22:32:41 +0530

    Loop

---
+++ loop.js
@@ -0,0 +1,29 @@
+// for (i = 0; i < 10; i++) {
+//   console.log(i);
+// }
+
+// let a = 10;
+
+// while (a < 12) {
+//   console.log("Chhota hai");
+//   a = a + 1;
+// }
+
+// let i = 1;
+
+// while (i <= 10) {
+//   console.log(i);
+//   i = i + 1;
+// }
+
+let number = 5;
+// let a = 1;
+
+// while (a <= 15) {
+//   console.log(number * a);
+//   a = a + 1;
+// }
+
+for (a = 1; a <= 10; a++) {
+  console.log(number * a);
+}
