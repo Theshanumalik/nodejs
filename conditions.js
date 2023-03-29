f9b8a63d88a6069a4a88e194f833348048ddce6d
theshanumalik <shanumalik9001567.com>
2023-03-29 21:49:07 +0530

    conditions

---
+++ conditions.js
@@ -0,0 +1,15 @@
+// IF ELSE CONDITIONS
+let a = 5;
+let b = "5";
+
+// OPRATORES ==, < , > , <=, >=, ===
+
+if (a === b) {
+  console.log("true");
+} else {
+  console.log("false");
+}
+
+// IF - ELSE IF - ELSE
+
+// SWITCH CASE
