0e1681739b340db9dfd906ef74d872029542e506
theshanumalik <shanumalik9001567.com>
2023-03-31 22:19:21 +0530

    day_4

---
+++ day_4.js
@@ -0,0 +1,29 @@
+console.log("hello world");
+let fruites = ["mango", "blackberry", "apple", "kiwi", "grapes"];
+
+// for (i = 0; i < fruites.length; i++) {
+//   document.write(`${i + 1}. ${fruites[i]}`);
+//   document.write("<br>");
+// }
+
+// fruites[0] = "strawberry";
+// fruites[5] = "banana";
+// fruites[6] = 56;
+
+// array ke last me add krne ke liye
+// fruites.push("banana", 5);
+
+// last element return krne ke liye
+// let lastElement = fruites.pop();
+
+// ek array se nya array bnane ke liye
+// let myArr = fruites.splice(1, 2); (startingIndex, count)
+
+//
+// let newArr = fruites.slice(1, 4);
+// console.log(newArr);
+
+// STRINGS
+// CALLBACKS
+// OBJECTS
+// WINDOW AND DOM
